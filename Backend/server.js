afec65b02961d9be73ab4a3999f304e5d6b916d8
Archer L Fox <71037555+DevoutSomething@users.noreply.github.com>
2025-10-25 11:06:15 +0800

    added skeleton for routing

--- Backend/server.js
+++ Backend/server.js
@@ -3,8 +3,52 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("API is up");
 });
+
+app.post("/userQuestionText", (req, res) => {
+  const prompt = req.body.prompt;
+
+  res.send({
+    promptReceived: prompt,
+    title: "What is the capital of France?",
+    answers: [
+      { answer: "Paris", isCorrect: true },
+      { answer: "London", isCorrect: false },
+      { answer: "Berlin", isCorrect: false },
+      { answer: "Madrid", isCorrect: false }
+    ]
+  });
+});
+
+
+app.post("/userQuestionQuiz", (req, res) => {
+  const prompt = req.body.prompt;
+
+  res.send({
+    promptReceived: prompt,
+    text: "not implimented yet"
+  });
+});
+
+
+app.post("/userQuestionVideo", (req, res) => {
+  const prompt = req.body.prompt;
+
+  res.send({
+    promptReceived: prompt,
+    videoUrl: "not implimented yet"
+  });
+});
+
+app.post("/updateUserSettings", (req, res) => {
+  const newSettings = req.body.settings;
+
+  res.send({
+    text: "settings have been updated"
+  });
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
